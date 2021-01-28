69ae52808dae03133d307e0e94ccd603050a364d
tole <tole.car@gmail.com>
2021-01-28 10:44:52 +0100

    log info

--- src/Convo/Pckg/Ta/TaBotUserCounterElement.php
+++ src/Convo/Pckg/Ta/TaBotUserCounterElement.php
@@ -31,7 +31,7 @@ class TaBotUserCounterElement extends \Convo\Core\Workflow\AbstractWorkflowCompo
     	parent::__construct( $properties);
     	
         $this->_statusVar   =   $properties['status_var'];
-        $this->_interval    =   $properties['interval_in_minutes'] * 60;
+        $this->_interval    =   $properties['interval_in_minutes'];
         $this->_taDao       =   $taDao;
     }
     
@@ -41,8 +41,12 @@ class TaBotUserCounterElement extends \Convo\Core\Workflow\AbstractWorkflowCompo
      */
     public function read( \Convo\Core\Workflow\IConvoRequest $request, \Convo\Core\Workflow\IConvoResponse $response)
     {
-        $status =   $this->_taDao->register( $request, $this->evaluateString( $this->_interval));
+        $interval   =   $this->evaluateString( $this->_interval) * 60;
+        $MIN    =   time() - $interval; 
         
+        $this->_logger->debug( 'Registering interval ['.$interval.']['.$MIN.']['.$request->getServiceId().']['.$request->getInstallationId().']');
+        $status =   $this->_taDao->register( $request, $interval);
+        $this->_logger->debug( 'Got status ['.$status.']');
         $param_name =   $this->evaluateString( $this->_statusVar);
         
         $this->getService()->getServiceParams( IServiceParamsScope::SCOPE_TYPE_SESSION)->setServiceParam( $param_name, $status);
